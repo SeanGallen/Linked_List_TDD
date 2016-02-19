fc75049425d83838b8612c4a0432dc047ca77b8e
Sean <sjgallen@gmail.com>
2016-02-19 08:18:06 -0600

    Linked_TDD

---
+++ array_symbol_test.rb
@@ -0,0 +1,21 @@
+require 'array_symbol'
+
+
+#A linked list is a nested collection of information.  It is like a stack.
+
+
+
+
+# [] returns the given value of an array's index.
+#
+#
+
+class AcceptanceTest < Minitest::Test
+
+ def test_array_link_list
+assert_equal nil,  list[0]
+assert_equal "b",  list[0]
+assert_equal nil, list[1]
+end
+
+end
